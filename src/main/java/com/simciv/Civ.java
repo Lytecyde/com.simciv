1880cacb85438f4e2edcd7b6d3f83d633feba0f1
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2018-04-11 23:12:20 +0300

    Added an init screen to gather start data.

--- src/main/java/com/simciv/Civ.java
+++ src/main/java/com/simciv/Civ.java
@@ -3,32 +3,61 @@ package com.simciv;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+
 public class Civ extends Application {
+    String[] playerName = {"",""};
     public static void main(String[] args) {
         launch(args);
     }
 
+
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Init");
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10,
+                10,
+                10,
+                10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        final TextField name = new TextField();
+        name.setPromptText("Enter your first name.");
+        name.setPrefColumnCount(10);
+        GridPane.setConstraints(name, 0, 0);
+        grid.getChildren().add(name);
+
+        TextField lastName = new TextField();
+        lastName.setPromptText("Enter your last name.");
+        GridPane.setConstraints(lastName, 0, 1);
+        grid.getChildren().add(lastName);
+
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Keep a record");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //record name
+                playerName[0] = name.getText();
+                playerName[1] = lastName.getText();
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        GridPane.setConstraints(btn, 1, 1);
+
+        grid.getChildren().add(btn);
+        primaryStage.setScene(new Scene(grid, 640, 480));
         primaryStage.show();
     }
 }
